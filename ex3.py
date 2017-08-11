e2d7c35c65b483bd4b8ddb30bbf06e77169cb475
rerreby <rerreby@rambler.ru>
2017-08-11 11:14:11 +0300

    Changes precise. int to float

--- ex3.py
+++ ex3.py
@@ -3,26 +3,26 @@ print "I will now count my chickens"
 
 # Calculate chickens, Hens and Roosters
 # and then somewhy we use "%" mod operation
-print "Hens", 25 + 30 / 6
-print "Roosters", 100 - 25 * 3 % 4
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
 
 # Print message about counting the egges
 print "Now, I will count the eggs"
 
 # ... and doing calculations itself
-print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
 
 # Print messages about trying calculation
 # the expression below
-print "Is it true that 3 + 2 < 7 - 5?"
+print "Is it true that 3.0 + 2.0 < 7.0 - 5.0?"
 
 # Doing the calculations itself
-print 3 + 2 < 7 - 5
+print 3.0 + 2.0 < 7.0 - 5.0
 
 # Since the result a bit confusing (False)
 # we need to perform the calculations step-by-step
-print "What's the 3 + 2", 3 + 2
-print "What's the 5 - 7", 5 - 7
+print "What's the 3.0 + 2.0", 3.0 + 2.0
+print "What's the 5.0 - 7.0", 5.0 - 7.0
 
 # "Evrika" message
 print "Oh! that's why it's False"
@@ -30,6 +30,6 @@ print "Oh! that's why it's False"
 # More examples about less-than great-than
 print "How about some more?"
 
-print "Is it greater?", 5 > -2              # True
-print "Is it greater or equal?", 5 >= -2    # True
-print "Is it less or equal", 5 <= -2        # False
+print "Is it greater?", 5.0 > -2.0              # True
+print "Is it greater or equal?", 5.0 >= -2.0    # True
+print "Is it less or equal", 5.0 <= -2.0        # False
